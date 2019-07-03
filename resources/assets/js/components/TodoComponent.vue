3f61b64e46aacf09cb6361c2cf9c4105b4442e5c
nishun0419 <ninosekunlight@gmail.com>
2019-07-04 00:28:22 +0900

    disptodo編集中

--- resources/assets/js/components/TodoComponent.vue
+++ resources/assets/js/components/TodoComponent.vue
@@ -3,10 +3,10 @@
 		<div class="row">
 			<div class="col-md-4" v-for="todo in todos">
 				<div class="panel panel-default">
-					<div class="panel-heading">@{{todo.title}}</div>
+					<div class="panel-heading">{{todo.title}}</div>
 
 					<div class="panel-body">
-						@{{todo.main}}
+						{{todo.main}}
 					</div>
 				</div>
 			</div>
